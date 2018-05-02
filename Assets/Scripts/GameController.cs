79de5f8249ad698b1e8c7c2ae282043482eed9d8
Sheerin Naseem <sheerin.naseem@gmail.com>
2018-05-02 16:31:04 +0530

    Guess my number game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+    [SerializeField]
+    private InputField input;
+
+    void Awake()
+    {
+        //input = GameObject.Find("InputField").GetComponent<InputField>();
+    }
+
+    public void GetInput(string guess)
+    {
+        Debug.Log("You Entered " + guess);
+        input.text = "";
+    }
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    // Use this for initialization
+    int max;
+    int min;
+    int guess;
+
+    void Start () {
+        StartGame();
+	}
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        print("============================");
+        print("Hi, Welcome to Number Wizard.");
+        print("Pick a number in your head but don't tell me.");
+
+        print("The highest number you can pick is " + max + " and the lowest number you can pick is " + min);
+        print("Press the up arrow key if it higher than 500 or press the down key if it lower than 500.");
+        print("Up = Higher, Down = Lower, Enter = Equal");
+
+        max = max + 1;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //print("Up key pressed.");
+            min = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            //print("Down key pressed.");
+            max = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("I won!!");
+            StartGame();
+        }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        print("Is the number higher or lower than " + guess);
+        print("Up = Higher, Down = Lower, Enter = Equal");
+    }
+}
